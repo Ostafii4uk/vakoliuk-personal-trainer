367c4ac947aed0dcfe443fa79f3986f307da2386
Ostafii4uk <ostafii4uk@gmail.com>
2023-08-24 20:26:34 +0300

    Added swipers

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
+const joinMePhotosSwiper = new Swiper(".join-me-photos-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 100,
+  autoplay: {},
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  }
+})
+
+const resultsPhotoSwiper = new Swiper(".results-photo-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 100,
+})
+
+const resultsFeedbackSwiper = new Swiper(".results-feedback-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 100,
+})
+
+const swiperButtonPrev = document.getElementById('swiper-button-prev')
+swiperButtonPrev.addEventListener('click', () => {
+  resultsPhotoSwiper.slidePrev(500, true)
+  resultsFeedbackSwiper.slidePrev(500, true)
+})
+
+const swiperButtonNext = document.getElementById('swiper-button-next')
+swiperButtonNext.addEventListener('click', () => {
+  resultsPhotoSwiper.slideNext(500, true)
+  resultsFeedbackSwiper.slideNext(500, true)
+})
+
 const body = document.getElementById('app')
 const playMeBtn = document.getElementById('play-about-me');
 const popupWindow = document.getElementById('header-popup');
